389786ce96dcb8ebb657f1dae0f92c8128b1a183
rama waleed <105355922+ramawaleed@users.noreply.github.com>
2022-12-15 16:05:12 +0200

    modifications
    سيتم العمل على انشاء باقي الصفحات الاخرى الخاصة بتسجيل العملاء في الموقع.
    ولقد أنجز جميع المحتوى المطلوب تعديله مسبقا.
    تبقى فقط المحتوى الذي ننتظره منكم وهو الخاص بصفحة آل    about us  .
    وبادن الله في ال meeting   القادم  سوف نلقي نظرة على الموقع بأكمله  وسيكون جميع الواجهات الخاصة بصفحات الموقع مكتملة بادن الله
    و سنقوم بتسجيل جميع ملاحظاتكم

---
+++ pay_myy/adsid/integrator.js
@@ -0,0 +1,7 @@
+processGoogleToken({
+    "newToken": "ChAIgNbLnAYQtIax_K3Hw8oMEjkA71JpnEjigydX7JBaXYI8krwFgcgU5CorKK7qK65bYYGinEBVry_cj1r2lPpSMie7ktt0QNZTXd0",
+    "validLifetimeSecs": 300,
+    "freshLifetimeSecs": 300,
+    "1p_jar": "",
+    "pucrd": ""
+});
